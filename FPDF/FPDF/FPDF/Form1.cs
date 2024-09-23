ec5d13a7842d487e469d3fa73aad9879fea48bab
Francesco Rombaldoni <f.rombaldoni@campus.uniurb.it>
2024-09-23 13:38:53 +0200

    continuation of the work

--- FPDF/FPDF/FPDF/Form1.cs
+++ FPDF/FPDF/FPDF/Form1.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 /*--New Imports--*/
 using System.IO;
+using iText;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
 
 namespace FPDF
 {
     public partial class FPDF : Form
     {
+
+        String path;
+
         public FPDF()
         {
             InitializeComponent();
@@ -22,7 +28,39 @@ namespace FPDF
 
         private void bNew_Click(object sender, EventArgs e)
         {
+             
 
+            using (OpenFileDialog ofd = new OpenFileDialog() { ValidateNames = true, Multiselect = false, Filter = "PDF|*.pdf" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                }
+            }
+
+            try 
+            {
+                MessageBox.Show(path);
+
+                using (PdfReader pdfReader = new PdfReader(path))
+                {
+                    using (PdfDocument pdfDocument = new PdfDocument(pdfReader))
+                    {
+                        for (int page = 1; page <= pdfDocument.GetNumberOfPages(); page++)
+                        {
+                            string currentText = PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(page));
+                            currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
+                            text.Append(currentText);
+                        }
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
